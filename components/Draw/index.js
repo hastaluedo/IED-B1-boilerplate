6bd50df96756ee58784522f7480a2aeb0f833da3
Edoardo Bordin <edo.bordin@gmail.com>
2021-06-16 17:39:22 +0200

    test 16-06

--- components/Draw/index.js
+++ components/Draw/index.js
@@ -1,16 +1,62 @@
 import p5 from 'p5';
 import component from './Draw.html';
+import "./draw.css;"
 
-function sketch(p) {
-  p.setup = () => {
-    p.createCanvas(800, 500);
-    p.background(0, 0, 0);
-  };
+const data = [
+  { id: 1, value: 60 },
+  { id: 1, value: 43 },
+  { id: 1, value: 46 },
+  { id: 1, value: 23 },
+  { id: 1, value: 93 },
+  { id: 1, value: 34 }
+];
 
-  p.draw = () => {
-    p.fill(255);
-    p.rect(30, 30, 30, 140);
-  };
+const step = 20;
+let divelems = [];
+
+function drawGraph() {
+  noFill();
+  stroke(125, 255, 100);
+  beginShape();
+  for (let i = 0; i < data.length; i++) {
+    const x = step * i;
+    const y = data[i].value;
+    vertex(x, y);
+  }
+  endShape();
+}
+
+function createDivs() {
+  for (let i = 0; i < data.length; i++) {
+    const x = step * i;
+    divelems[i] = createDiv();
+    divelems[i].position(x, 0);
+    divelems[i].parent('grafo-uno')
+    divelems[i].size(step, 400);
+    divelems[i].style("border: 2px dashed");
+    divelems[i].mouseOver(function changeColor() {
+      divelems[i].style("background-color: lightgreen");
+    });
+    divelems[i].mouseOut(function changeColor() {
+      divelems[i].style("background-color: transparent");
+    });
+  }
+}
+
+function changeColor(i) {
+  divelems[i].style("background-color: lightgreen");
+}
+
+function setup() {
+  const canvas = createCanvas(600, 400);
+  canvas.parent('grafo-uno')
+  background(125);
+  createDivs();
+}
+
+function draw() {
+  background(125);
+  drawGraph();
 }
 
 new p5(sketch, component);
